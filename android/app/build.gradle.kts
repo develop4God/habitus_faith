ce001d42391d34060f0e712bc315d21d75576c99
develop4God <develop4God@gmail.com>
2025-10-07 22:05:06 -0500

    feat: Rename project to habitus_faith
    Renames the project from `habitus_fe` to `habitus_faith` and updates all related configuration files, including:
    - `pubspec.yaml` and `pubspec.lock`
    - Android manifest and build files (`AndroidManifest.xml`, `build.gradle.kts`, `MainActivity.kt`)
    
    Dependency versions in `pubspec.lock` and the Dart SDK constraint have also been updated.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     ndkVersion = "27.0.12077973"
-    namespace = "com.example.habitus_fe"
+    namespace = "com.develop4God.habitus_faith"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,10 +21,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.habitus_fe"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
+        applicationId = "com.develop4God.habitus_faith"
+
+
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
---
+++ android/app/src/main/kotlin/com/develop4God/habitus_faith/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.develop4God.habitus_faith
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
